1d6a98cf918caece23cd0cd406c697a5214dc1a2
Antonio Ojea <aojea@google.com>
2026-06-09 18:26:39 +0000

    also mount kubelet device plugin

--- pydra/core/server.py
+++ pydra/core/server.py
@@ -430,8 +430,9 @@ class DraNodeServer(dra_pb2_grpc.DRAPluginServicer, deviceplugin_pb2_grpc.Device
         if self.enable_device_plugin:
             server_dp = grpc.aio.server()
             deviceplugin_pb2_grpc.add_DevicePluginServicer_to_server(self, server_dp)
-            dp_socket = f"{self.socket_path}-legacy"
-            dp_kubelet_socket = f"{self.kubelet_socket_path}-legacy"
+            dp_socket_name = f"{self.plugin_name.replace('/', '-')}-legacy.sock"
+            dp_socket = f"/var/lib/kubelet/device-plugins/{dp_socket_name}" if "/var/lib/kubelet" in self.socket_path else os.path.join(os.path.dirname(self.socket_path), dp_socket_name)
+            dp_kubelet_socket = dp_socket_name
             
             reg_dp = RegistrationWrapper("DevicePlugin", self.plugin_name, dp_kubelet_socket, ["v1beta1"], self.logger)
             reg_pb2_grpc.add_RegistrationServicer_to_server(reg_dp, server_dp)
