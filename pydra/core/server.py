215773d553e180b1c0c1fc01d6799ec208cdd8d4
Antonio Ojea <aojea@google.com>
2026-06-09 19:55:59 +0000

    Fix unused variable in server.py

--- pydra/core/server.py
+++ pydra/core/server.py
@@ -430,7 +430,6 @@ class DraNodeServer(dra_pb2_grpc.DRAPluginServicer, deviceplugin_pb2_grpc.Device
             deviceplugin_pb2_grpc.add_DevicePluginServicer_to_server(self, server_dp)
             dp_socket_name = f"{self.plugin_name.replace('/', '-')}-legacy.sock"
             dp_socket = f"/var/lib/kubelet/device-plugins/{dp_socket_name}" if "/var/lib/kubelet" in self.socket_path else os.path.join(os.path.dirname(self.socket_path), dp_socket_name)
-            dp_kubelet_socket = dp_socket_name
             dp_resource_name = f"{self.plugin_name}/device"
             
             reg_dp = RegistrationWrapper("DevicePlugin", dp_resource_name, dp_socket, ["v1beta1"], self.logger)
